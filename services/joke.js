15b8c0f046d2c0008f84c7f55f9a1aa4f2feddd5
const-z <->
2017-04-21 03:20:42 +0400

    joke source

--- services/joke.js
+++ services/joke.js
@@ -6,14 +6,25 @@ const sanitizeHtml = require("sanitize-html");
 class JokeService {
 
 	constructor() {
+		this.sites = [
+			{
+				"site": "anekdot.ru",
+				"name": "new anekdot"
+			},
+			{
+				"site": "bash.im",
+				"name": "bash"
+			}
+		];
 		this.collection = [];
 	}
 
 	async get() {
 		if (this.collection.length === 0) {
-			let response = await fetch("http://www.umori.li/api/random?num=100");
-			// let response = await fetch("http://www.umori.li/api/get?site=bash.im&name=bash&num=100");
-			this.collection = await response.json();
+			for (let site of this.sites) {
+				let response = await fetch("http://www.umori.li/api/get?site=" + site.site + "&name=" + site.name + "&num=100");
+				this.collection = this.collection.concat(await response.json());
+			}
 		}
 		let joke = this.collection.pop();
 		let msg = sanitizeHtml(joke.elementPureHtml, { allowedTags: [], allowedAttributes: {} }) + "\n\n*" + joke.site + " - " + joke.desc + "* :)";
